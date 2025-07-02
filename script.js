0a615e8c09b2a124f01766831ed432138f1a4132
rafaelgoncalves1994 <rafaelsergio1994@gmail.com>
2025-07-02 14:15:12 -0300

    site

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+  const height = card.clientHeight;
+  const width = card.clientWidth;
+
+  card.addEventListener('mousemove', e => {
+    const xVal = (e.offsetX - width / 2) / width * 20;
+    const yVal = -(e.offsetY - height / 2) / height * 20;
+    card.style.transform = `rotateY(${xVal}deg) rotateX(${yVal}deg)`;
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  });
+});
