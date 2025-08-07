70a427835b67851c914490b5ce201731fdf95a72
rafaelgoncalves1994 <rafaelsergio1994@gmail.com>
2025-08-07 20:43:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-
-function decodeEffect(
-  element,
-  finalText,
-  speed = 50,
-  charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*"
-) {
-  let iterations = 0;
-  const length = finalText.length;
-
-  const interval = setInterval(() => {
-    let output = "";
-
-    for (let i = 0; i < length; i++) {
-      if (i < iterations) {
-        output += finalText[i];
-      } else {
-        output += charset[Math.floor(Math.random() * charset.length)];
-      }
-    }
-
-    element.textContent = output;
-
-    if (iterations >= length) {
-      clearInterval(interval);
-      element.textContent = finalText;
-    }
-
-    iterations += 1;
-  }, speed);
-}
-
 function initCardTilt() {
   const cards = document.querySelectorAll(".card");
 
@@ -49,26 +17,4 @@ function initCardTilt() {
   });
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  const hero = document.getElementById("hero");
 
-  if (hero) {
-    const h1 = hero.querySelector("h1");
-    const p = hero.querySelector("p");
-
-    if (h1 && p) {
-      const h1Text = h1.textContent;
-      const pText = p.textContent;
-
-    
-      h1.textContent = "";
-      p.textContent = "";
-
-      
-      decodeEffect(h1, h1Text, 80);
-      setTimeout(() => decodeEffect(p, pText, 70), 1200);
-    }
-  }
-
-  initCardTilt();
-});
