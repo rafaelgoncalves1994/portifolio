6fe54928016a0492def9bf4408b5a5555e4e0163
rafaelgoncalves1994 <rafaelsergio1994@gmail.com>
2025-08-07 22:04:44 -0300

    testes

--- script.js
+++ script.js
@@ -5,16 +5,20 @@ function initCardTilt() {
     const height = card.clientHeight;
     const width = card.clientWidth;
 
+    card.style.transition = "transform 0.15s ease";
+
     card.addEventListener("mousemove", (e) => {
       const xVal = ((e.offsetX - width / 2) / width) * 20;
       const yVal = (-(e.offsetY - height / 2) / height) * 20;
+
+      card.style.transition = "transform 0.05s ease-out";
+
       card.style.transform = `rotateY(${xVal}deg) rotateX(${yVal}deg)`;
     });
 
     card.addEventListener("mouseleave", () => {
+      card.style.transition = "transform 0.3s ease";
       card.style.transform = `rotateY(0deg) rotateX(0deg)`;
     });
   });
 }
-
-
